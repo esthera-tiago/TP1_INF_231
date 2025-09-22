297a59f9f7a3f4176c28537f35768df6004433f6
Lembou Pharel <lemboupharel@proton.me>
2025-09-22 15:25:19 +0100

    Ajoute recherche sequentiel et teste a de tri a tableaux

--- tableaux/recherche_seq.c
+++ tableaux/recherche_seq.c
@@ -1,1 +1,20 @@
-test
+#include <stdio.h>
+#define T_MAX 1000
+int main (int argc, char *argv[]){
+    int A[T_MAX];
+    int n, val;
+    printf("\nEntre le nombre d'elements du tableaux: ");
+    scanf("%d", &n);
+    printf("\nEntre les valeurs du tableaux: ");
+
+    for(int i=0; i<n; i++)
+        scanf("%d", &A[i]);
+
+    printf("Entre la valeur a rechercher: ");
+    scanf("%d", &val);
+    for(int i=0; i<n; i++){
+        A[i] != val ?  : printf("valeur trouver a position %d du tableaux\n", i) ;
+    }
+
+    return 0;
+}
--- tableaux/test_tri.c
+++ tableaux/test_tri.c
@@ -1,1 +1,20 @@
-test
+#include <stdio.h>
+#define T_MAX 1000
+int main (int argc, char *argv[]){
+    int A[T_MAX];
+    unsigned int n, asc=0, desc=0;
+    printf("\nEntre le nombre d'elements du tableaux: ");
+    scanf("%d", &n);
+    printf("\nEntre les valeurs du tableaux: ");
+
+    for(int i=0; i<n; i++)
+        scanf("%d", &A[i]);
+
+
+    for(int i=0; i<n-1; i++)
+        A[i] <= A[i+1]  ?  (asc+=1) : (desc+=1);
+
+    asc == n-1 ? printf("\nTableaux trie pars ordre croisant.") : (desc == n-1 ? printf("\nTableaux trie pars ordre decroisant.") : printf("Tableaux pas trie"));
+
+    return 0;
+}
